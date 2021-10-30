f57410932fa1c60f2c89174ad16df1409fffa3f0
Brinton <brintonfoyreed@gmail.com>
2021-10-30 12:11:48 -0700

    Basic functionality working

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,27 +1,52 @@
 function selectionSort(arr) {
-  
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > arr[i+1]) {
-      
-    }
+  let startTime = new Date()
+  console.log("start time: ", startTime)
+  let newArr = []
+  let counter = arr.length
+  for (let i = 0; i < counter; i++) {
+    let min = Math.min(...arr)
+    let index = arr.indexOf(min)
+    arr.splice(index, 1)
+    newArr.push(min)
+    console.log("min: ", min, "index: ", index, "arr:", newArr)
   }
-
+  console.log("end time: ", startTime)
+  return newArr
 }
 
 if (require.main === module) {
-  // add your own tests in here
+
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSort([3, -1, 5, 2]));
 
   console.log("");
 
+  console.log("Expecting: [ 2, 3, 4, 5, 6]")
+  console.log("=>", selectionSort([4,5,3,6,2]))
+
   // BENCHMARK HERE, and print the average runtime
+  
   const longInput = [];
 
   for (let i = 0; i < 100; ++i) {
     longInput.push(Math.random());
   }
-}
+
+  const longerInput = [];
+
+  for (let i = 0; i < 1000; ++i) {
+    longInput.push(Math.random());
+  }
+
+  console.log("longInput/100x: ", selectionSort(longInput) )
+  console.log("longerInput/1000x: ", selectionSort(longInput) )
+
+//   store the current time in a variable called start time
+// loop 1000 times:
+//   execute the method using a small input, e.g. three items if input is an Array
+//   execute the method using a larger input, e.g. 100 items if input is an Array
+// average runtime = (current time - start time) / 2000
+
 
 module.exports = selectionSort;
 
@@ -29,3 +54,5 @@ module.exports = selectionSort;
 then unshifting it to the front of the array. Repeat
 until iterations = arr.length.
 */ 
+
+}
